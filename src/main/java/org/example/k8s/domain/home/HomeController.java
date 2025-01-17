355bc3910ab463fcdc666050deff4c5b723f4db9
LSH-kw0315 <tmdgjsdl1234@naver.com>
2025-01-17 14:47:13 +0900

    fix: fix showMain

--- src/main/java/org/example/k8s/domain/home/HomeController.java
+++ src/main/java/org/example/k8s/domain/home/HomeController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class HomeController {
     @Value("${custom.jwt.secretKey}")
@@ -11,8 +14,11 @@ public class HomeController {
 
 
     @GetMapping("/")
-    public String home(){
-        return "Hello, World!";
+    public String showMain() throws UnknownHostException {
+        InetAddress localHost = InetAddress.getLocalHost();
+        // getHostName 메소드를 호출하여 호스트 이름을 얻습니다.
+        String hostname = localHost.getHostName();
+        return "home, hostname : " + hostname;
     }
 
     @GetMapping("/jwtSecretKey")
